8c528aab3ee089fc8538dd1780c7ea54dec988d0
Rafael Stauffer <rafael.staffer@hotmail.de>
2023-06-20 20:01:20 +0200

    feat: add blog get

--- blog-backend/src/main/java/ch/hftm/API/BlogResource.java
+++ blog-backend/src/main/java/ch/hftm/API/BlogResource.java
@@ -1,16 +1,32 @@
 package ch.hftm.API;
 
+import java.util.List;
+
+import ch.hftm.Entities.Blog;
+import ch.hftm.Repositories.BlogRepository;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/blog")
+@Path("blogs")
 public class BlogResource {
 
+    @Inject
+    BlogRepository blogRepository;
+
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String blog() {
-        return "Willkommen auf meinem Blog!";
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Blog> blogs() {
+        return blogRepository.getBlogs();
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Blog blog(@PathParam("id") Long id) {
+        return blogRepository.findById(id);
     }
 }
